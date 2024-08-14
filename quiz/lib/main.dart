56dc8d56ad88ffa0292f7f1691ccbbfe2aa55055
muhunXD <muhun2548@gmail.com>
2024-08-14 23:17:36 +0700

    Basic quiz app with In-class question

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -1,125 +1,196 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quiz App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: QuizPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class QuizPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _QuizPageState createState() => _QuizPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _QuizPageState extends State<QuizPage> {
+  final List<Question> _questions = [
+    Question(
+        questionText:
+            'การรวมเลขของระบบ 2’s complement 2 + 7 ควรได้เลขฐานสอง จำนวน 5 บิตเป็นเท่าไร',
+        answers: {
+          'ก. 00110': false,
+          'ข. 01001': false,
+          'ค. 10000': true,
+          'ง. 00111': false,
+        }),
+    Question(
+        questionText: 'Canonical sum ของ AB + AbarB ควรอ่านว่าอย่างไร',
+        answers: {
+          'ก. A and B or A bar and B': true,
+          'ข. A or B and A bar or B': false,
+          'ค. A and B or A and B': false,
+          'ง. ถูกทุกข้อ': false,
+        }),
+    Question(
+        questionText:
+            'จากเลขฐานสอง 101011 เมื่อแปลงเป็นเลขฐานสิบแล้ว ค่าที่ได้ตรงกับข้อใด',
+        answers: {
+          'ก. 30₁₀': false,
+          'ข. 43₁₀': false,
+          'ค. 45₁₀': true,
+          'ง. 47₁₀': false,
+        }),
+    Question(
+        questionText:
+            'Maurice Karnaugh คือผู้คิดหลักการลดรูปสมการบูลีนด้วยวิธีอะไร',
+        answers: {
+          'ก. Karnaugh Map': true,
+          'ข. Karna Map': false,
+          'ค. Karnung Map': false,
+          'ง. Kaning Map': false,
+        }),
+    Question(
+        questionText:
+            'จงแปลงกลับ 10’s compliment ของเลขฐานสองดังต่อไปนี้ -01110 (-14)',
+        answers: {
+          'ก. 951': false,
+          'ข. 231': false,
+          'ค. 064': true,
+          'ง. 085': false,
+        }),
+    Question(
+        questionText:
+            'จงแปลงกลับ 2 compliment ของเลขฐาน10ดังต่อไปนี้ 16(10000)',
+        answers: {
+          'ก. 00010': false,
+          'ข. 00001': true,
+          'ค. 101111': false,
+          'ง. 10001': false,
+        }),
+  ];
 
-  void _incrementCounter() {
+  int _currentQuestionIndex = 0;
+  int _score = 0;
+
+  void _answerQuestion(bool isCorrect) {
+    if (isCorrect) {
+      setState(() {
+        _score++;
+      });
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentQuestionIndex++;
+    });
+
+    if (_currentQuestionIndex >= _questions.length) {
+      _showResultDialog();
+    }
+  }
+
+  void _showResultDialog() {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Quiz Completed!'),
+        content: Text('Your score is $_score out of ${_questions.length}'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop();
+              _resetQuiz();
+            },
+            child: Text('Restart Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _resetQuiz() {
+    setState(() {
+      _currentQuestionIndex = 0;
+      _score = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Quiz App'),
+        backgroundColor: Colors.green, // Change this to any color you like
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: _currentQuestionIndex < _questions.length
+          ? Quiz(
+              question: _questions[_currentQuestionIndex],
+              answerQuestion: _answerQuestion,
+            )
+          : Center(
+              child: Text('You did it! Your score is $_score'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Quiz extends StatelessWidget {
+  final Question question;
+  final Function(bool) answerQuestion;
+
+  Quiz({required this.question, required this.answerQuestion});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                question.questionText,
+                style: TextStyle(fontSize: 24),
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(height: 20),
+              ...question.answers.keys.map((answer) {
+                return Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.only(bottom: 10),
+                  child: ElevatedButton(
+                    onPressed: () => answerQuestion(question.answers[answer]!),
+                    child: Text(answer),
+                    style: ElevatedButton.styleFrom(
+                      alignment: Alignment.centerLeft,
+                      padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Question {
+  final String questionText;
+  final Map<String, bool> answers;
+
+  Question({required this.questionText, required this.answers});
 }
